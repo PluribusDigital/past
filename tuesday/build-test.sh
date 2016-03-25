2b1956c93acc02379dda5d08fd74c51f7fda738e
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2016-03-25 11:35:10 -0400

    Manually reseeding the tuesday database with the prior art documents

--- tuesday/build-test.sh
+++ tuesday/build-test.sh
@@ -5,7 +5,7 @@ docker build -t tuesday-test-db \
 -f /home/vagrant/tuesday/db/Dockerfile-testing \
 /home/vagrant/tuesday/db
 
-docker run -it -p "5432:5432" \
+docker run -it -p "2345:5432" \
 --env-file /home/vagrant/.env \
 -e "POSTGRES_DB=tuesday" \
 -v "/var/lib/postgresql/tuesday-test-data:/var/lib/postgresql/data" \
